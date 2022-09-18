b5b83cd53cfb8119a82742123868bb989ba2b76f
tjshee39 <gksh054@naver.com>
2022-09-18 16:34:45 +0900

    20220918_board UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from 'images/logo.png';
+import BoardList from 'components/BoardList';
 
 function App() {
-  return (
-    <div className="App">
-      react project
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="banner">
+                <img src={logo} className="logo_img" />
+            </div>
+            <BoardList></BoardList>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/BoardList.tsx
@@ -0,0 +1,70 @@
+//import { render } from '@testing-library/react';
+import { Component } from 'react';
+import '../css/boardContent.css';
+import Table from 'react-bootstrap/Table';
+
+/**
+ * boardList class
+ */
+class BoardList extends Component {
+    /**
+     * @return { Component } Component
+     */
+    render() {
+        return (
+            <Table className="content_table">
+                <tr>
+                    <td>
+                        <BoardContent />
+                    </td>
+                    <td>
+                        <BoardContent />
+                    </td>
+                    <td>
+                        <BoardContent />
+                    </td>
+                    <td>
+                        <BoardContent />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <BoardContent />
+                    </td>
+                    <td>
+                        <BoardContent />
+                    </td>
+                    <td>
+                        <BoardContent />
+                    </td>
+                    <td>
+                        <BoardContent />
+                    </td>
+                </tr>
+            </Table>
+        );
+    }
+}
+
+class BoardContent extends Component {
+    /**
+     * @return { Component } Component
+     */
+    render() {
+        return (
+            <Table>
+                <tr className="content_img">
+                    <td>사진</td>
+                </tr>
+                <tr>
+                    <td>로우앤슬로우</td>
+                </tr>
+                <tr>
+                    <td className="content_review_rating">★ 4.7</td>
+                </tr>
+            </Table>
+        );
+    }
+}
+
+export default BoardList;
