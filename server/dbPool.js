7afc3de2e8973e38c9463f0cc806ab1bd464dcec
tjshee39 <gksh054@naver.com>
2023-09-16 15:34:02 +0900

    배포 db 외부접속

--- server/dbPool.js
+++ server/dbPool.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
 
 const db = mysql.createPool({
-    host: "122.45.13.226:3306",
+    host: "122.45.13.226",
     user: "usr_somet",
     password: "1234",
     database: "sometaste",
-  });
+    port: "3306"
+});
 
 db.getConnection(function(err, conn) {
     if(err) console.log(err);
