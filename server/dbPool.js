0ebac580fcfac0f521017a20e0c15da600c3de76
tjshee39 <gksh054@naver.com>
2023-09-18 13:13:26 +0900

    db수정

--- server/dbPool.js
+++ server/dbPool.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
 
-// const db = mysql.createPool({
-//     host: "122.45.13.226",
-//     user: "usr_somet",
-//     password: "1234",
-//     database: "sometaste",
-//     port: "3306"
-// });
-
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
+    host: "122.45.13.226",
+    user: "usr_somet",
     password: "1234",
     database: "sometaste",
+    port: "3306"
 });
+
+// const db = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "1234",
+//     database: "sometaste",
+// });
 
 db.getConnection(function(err, conn) {
     if(err) console.log(err);
